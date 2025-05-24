3ca834ba57d96fe13fd9db9f9bf9eb284aa0f758
Dlafferty251 <dlafferty251@gmail.com>
2025-05-24 16:27:57 -0400

    feat: creates rotating quote feature and also revamps about section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
-import './globals.css';
-import Header from '../components/Header/Header';
-import { ReactNode } from 'react';
+import "./globals.css";
+import Header from "../components/Header/Header";
+import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
